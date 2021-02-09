cc8045f36a8be9bd9046014d8e7b128e841b795a
Ivan Saldano <ivanms1@gmail.com>
2021-02-09 16:14:07 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const { data, isFetching, isError, refetch } = useQuery("food", () =>
+    axios.get("https://foodish-api.herokuapp.com/api/")
+  );
+
+  if (isError) {
+    return <p>Oops an error happened</p>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Food Generator</h1>
+      <button className="button" type="button" onClick={() => refetch()}>
+        Generate
+      </button>
+      {isFetching ? (
+        <p>Loading...</p>
+      ) : (
+        <img className="image" src={data?.data?.image} alt="food" />
+      )}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import "./index.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 3600,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
